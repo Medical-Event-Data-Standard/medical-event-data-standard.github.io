13c7051fadf1b147354ab850f6bf2043030a9045
Matthew McDermott <mattmcdermott8@gmail.com>
2024-11-23 19:28:32 -0500

    Added deployment instructions and updated deployment branch.

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -20,7 +20,7 @@ const config: Config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'Medical-Event-Data-Standard', // Usually your GitHub org/user name.
   projectName: 'medical-event-data-standard.github.io', // GitHub repo
-
+  deploymentBranch: 'gh-pages',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
