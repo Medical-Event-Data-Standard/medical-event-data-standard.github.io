08e1f1e1d85eaab9c63ec56b13c953ca0f76e10e
Matthew McDermott <mattmcdermott8@gmail.com>
2025-03-13 17:14:04 -0400

    A different approach that seems to work a bit better.

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -66,6 +66,8 @@ const config: Config = {
     ],
   ],
 
+  //plugins: ['@datalayer/jupyter-docusaurus-plugin'],
+
   themeConfig: {
     // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
