0551546d03ddf6c40aa5452c19375efdbef047b4
Matthew McDermott <mattmcdermott8@gmail.com>
2025-05-20 11:12:30 -0400

    Added prettier code formatting

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -1,49 +1,47 @@
-import clsx from "clsx";
-import Heading from "@theme/Heading";
-import styles from "./styles.module.css";
+import clsx from 'clsx';
+import Heading from '@theme/Heading';
+import styles from './styles.module.css';
 
-import shockinglySimpleSvg from "@site/static/img/shockingly_simple.svg";
-import builtForAiSvg from "@site/static/img/built_for_AI.svg";
-import frictionlessReproducibilityPng from "@site/static/img/frictionless_reproducibility.png";
+import shockinglySimpleSvg from '@site/static/img/shockingly_simple.svg';
+import builtForAiSvg from '@site/static/img/built_for_AI.svg';
+import frictionlessReproducibilityPng from '@site/static/img/frictionless_reproducibility.png';
 
 type FeatureItem = {
   title: string;
-  Svg: React.ComponentType<React.ComponentProps<"svg">>;
+  Svg: React.ComponentType<React.ComponentProps<'svg'>>;
   description: JSX.Element;
 };
 
 const FeatureList: FeatureItem[] = [
   {
-    title: "Shockingly Simple",
+    title: 'Shockingly Simple',
     Svg: shockinglySimpleSvg,
     PNG: null,
     description: (
       <>
-        MEDS (Medical Event Data Standard) is the simplest possible standard for
-        health AI, making it easy to use with diverse data sources and modelling
-        needs.
+        MEDS (Medical Event Data Standard) is the simplest possible standard for health AI, making it easy to
+        use with diverse data sources and modelling needs.
       </>
     ),
   },
   {
-    title: "Health AI Ecosystem",
+    title: 'Health AI Ecosystem',
     Svg: builtForAiSvg,
     PNG: null,
     description: (
       <>
-        MEDS empowers a high-performance and flexible health AI ecosystem to
-        streamline your research via parallelism and sparse data structures.
+        MEDS empowers a high-performance and flexible health AI ecosystem to streamline your research via
+        parallelism and sparse data structures.
       </>
     ),
   },
   {
-    title: "Frictionless Reproducibility",
+    title: 'Frictionless Reproducibility',
     PNG: frictionlessReproducibilityPng,
     description: (
       <>
-        MEDS ensures that your models, tools, and pipelines are easily
-        reproducible across data sites, research tasks, and computational
-        environments.
+        MEDS ensures that your models, tools, and pipelines are easily reproducible across data sites,
+        research tasks, and computational environments.
       </>
     ),
   },
@@ -51,7 +49,7 @@ const FeatureList: FeatureItem[] = [
 
 function Feature({ title, Svg, PNG, description }: FeatureItem) {
   return (
-    <div className={clsx("col col--4")}>
+    <div className={clsx('col col--4')}>
       <div className="text--center">
         {PNG && <img src={PNG} className={styles.featurePng} alt={title} />}
         {Svg && <Svg className={styles.featureSvg} />}
